da19f563a8968f751b966ec200835fd8a044fb08
andrew1931 <andrewk1931@gmail.com>
2022-05-22 14:22:21 +0200

    feat: improve ellipse drawing

--- projects/ngx-canvas-drawboard/src/lib/shapes/ellips.ts
+++ projects/ngx-canvas-drawboard/src/lib/shapes/ellips.ts
@@ -9,14 +9,20 @@ export class Ellips extends BaseShape {
     if (props.ctx === null) {
       return;
     }
-		props.ctx.beginPath();
+
     const { x, y, width, height } = props.elem;
     const mouseX = width + x;
     const mouseY = height + y;
 
-    props.ctx.moveTo(x, y + (mouseY - y) / 2);
-    props.ctx.bezierCurveTo(x, y, mouseX, y, mouseX, y + (mouseY - y) / 2);
-    props.ctx.bezierCurveTo(mouseX, mouseY, x, mouseY, x, y + (mouseY - y) / 2);
+    const scaleX = 1 * ((mouseX - x) / 2);
+    const scaleY = 1 * ((mouseY - y) / 2);
+    const centerX = (x / scaleX) + 1;
+    const centerY = (y / scaleY) + 1;
+    props.ctx.save();
+		props.ctx.beginPath();
+    props.ctx.scale(scaleX ,scaleY);
+    props.ctx.arc(centerX, centerY, 1, 0, 2 * Math.PI);
+    props.ctx.restore();
 
     super.drawElemet(props);
 	};
